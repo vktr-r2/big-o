3e0ff6b0fd25651a47e0e7c8535106cb0b725b1a
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-15 23:44:59 -0400

    Add O(n^2) quadratic complexity notes

--- quadratic_time.py
+++ quadratic_time.py
@@ -20,3 +20,35 @@ def sum_product_pairs(list):
 for size in input_sizes:
   runtime = measure_runtime(sum_product_pairs, size)
   print(f"Input size: {size}, Runtime:{runtime:.20f} seconds\n")
+
+
+"""
+NOTES
+
+O(n^2)
+- O(n^2) is also known as quadratic time complexity.
+
+- Quadratic time sees an increase in time that is proportional to the square of the data set.
+
+- Quadratic time takes a lot longer as data sets get bigger.  This kind of runtime is actually common.  An example is a nested loop.
+  => The inner loop looks at each element for every element in the outer loop.  Such an algorithm has quadratic time complexity whiche makes it quite slow
+
+REAL WORLD EXAMPLE
+
+Suppose you're building a graphics application that needs to transform 2D points using a transformation matrix. For a 2x2 matrix, which is relatively small, a nested loop to perform the matrix-vector multiplication might be acceptable.
+
+def matrix_vector_multiply(matrix, vector):
+    result = [0, 0]
+    for i in range(len(matrix)):
+        for j in range(len(vector)):
+            result[i] += matrix[i][j] * vector[j]
+    return result
+
+transformation_matrix = [[2, 0], [0, 2]]  # 2x2 transformation matrix
+point = [3, 4]  # 2D point
+
+transformed_point = matrix_vector_multiply(transformation_matrix, point)
+print("Transformed Point:", transformed_point)
+
+
+"""

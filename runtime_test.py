d4962bf7683677e634740e8f8d706b7a1743ff77
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-16 00:22:17 -0400

    Add test for functions that take single num as arguement

--- runtime_test.py
+++ runtime_test.py
@@ -3,21 +3,35 @@ import time
 
 def measure_runtime(function, input_size):
     
-    # create list that will range from 0 - input_size
-    input_data = list(range(input_size))
+  # create list that will range from 0 - input_size
+  input_data = list(range(input_size))
 
-    # capture start time in variable
-    start_time = time.time()
+  # capture start time in variable
+  start_time = time.time()
 
-    # run argument function on input_data
-    function(input_data)
+  # run argument function on input_data
+  function(input_data)
 
-    # capture end time in variable
-    end_time = time.time()
+  # capture end time in variable
+  end_time = time.time()
 
-    # return the total run time
-    return end_time - start_time
+  # return the total run time
+  return end_time - start_time
 
+
+def measure_runtime_single_input(function, input):
+  
+  # capture start time in variable
+  start_time = time.time()
+
+  # run argument function on input_data
+  function(input)
+
+  # capture end time in variable
+  end_time = time.time()
+
+  # return the total run time
+  return end_time - start_time
 
 
 """
